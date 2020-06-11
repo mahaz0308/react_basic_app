2d6ceb499c1f4ef8eb303d00beb184f1fca6f8dc
Mahaz abbasi <mahazabbasi070@gmail.com>
2020-06-11 23:52:46 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      hi mahaz
+    </div>
+  );
+}
+
+export default App;
